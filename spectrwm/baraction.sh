b756093a77bf09e7b00c3f6b32ae0dd44b6bd309
Dev_Larks <Dev_Larks@Outlook.com>
2021-04-01 23:15:34 +1100

    Add Font Awesome icons

--- spectrwm/baraction.sh
+++ spectrwm/baraction.sh
@@ -10,7 +10,7 @@
 
 hdd() {
 	  hdd="$(df -h | awk 'NR==4{print $3 " / " $5}')"
-	    echo -e "HDD: $hdd"
+	    echo -e ": : $hdd"
     }
 
 ##############################
@@ -25,7 +25,7 @@ totalh="$(free -h | grep Mem: | awk '{print $2}' | sed 's/Gi/G/')"
 
 ram="$(( 200 * $used/$total - 100 * $used/$total ))% / $totalh "
 
-echo RAM: $ram
+echo : : $ram
 }
 
 
@@ -40,13 +40,14 @@ cpu() {
 	        read cpu a b c idle rest < /proc/stat
 		  total=$((a+b+c+idle))
 		    cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-		      echo -e " CPU: $cpu% "
-	      }
+		     # echo -e " CPU: $cpu% "
+		     echo -e "   : $cpu%"
+}
 
 temp() {
   eval $(sensors | awk '/^Core 0/ {gsub(/°/,""); printf "CPU0=%s;", $3}')
   eval $(sensors | awk '/^Core 1/ {gsub(/°/,""); printf "CPU1=%s;", $3}')
-  echo -e "CPU TEMP: ${CPU0} / ${CPU1}" 
+  echo -e " : ${CPU0} / ${CPU1}" 
 }
 
 
@@ -56,14 +57,39 @@ temp() {
 
 vol() {
 	vol=`amixer get Master | awk -F'[][]' 'END{ print $2 }' | sed 's/on://g'`
-	echo -e "VOL: $vol"
+	echo -e ": $vol"
 }
 
+
+#############################
+#	UPDATES
+#############################
+
+#updates() {
+#	pacman -Sy 2> /dev/null
+#	
+#	if ! updates_arch=$(pacman -Qu 2> /dev/null | wc -l ); then
+#	updates_arch=0
+#	fi
+#
+#	if ! updates_aur=$(paru -Qua 2> /dev/null | wc -l); then
+#   	updates_aur=0
+#	fi
+#
+#	updates=$(("$updates_arch" + "$updates_aur"))
+#
+#	if [ "$updates" -gt 0 ]; then
+#	    echo " : $updates"
+#	else
+#	    echo "0"
+#	fi
+#}
 
 
 SLEEP_SEC=2	
 #loops forever outputting a line every SLEEP_SEC secs
 	while :; do     
 		echo "+@fg=4; $(cpu) +@fg=0; | +@fg=2; $(temp) +@fg=0; | +@fg=1; $(mem) +@fg=0; | +@fg=3; $(hdd) +@fg=0; | +@fg=7; $(vol) +@fg=0; |"
+		       #	+@fg=0; $(updates) +@fg=0;"
 		sleep $SLEEP_SEC
 	done
